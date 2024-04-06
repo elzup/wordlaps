80d26bba5b72f8bcaedc3df040625fbf7eadb4b4
elzup <guild0105@gmail.com>
2024-04-06 11:30:25 +0900

    add copy db

---
+++ copy_db.sh
@@ -0,0 +1,8 @@
+
+
+# Mac
+
+cp ~/Library/Application\ Support/Google/Chrome/Default/History ./db/
+cp ~/Library/Application\ Support/Google/Chrome/Profile\ 3/History ./db/History-3
+cp ~/Library/Application\ Support/Google/Chrome/Profile\ 1/History ./db/History-5
+cp ~/Library/Application\ Support/com.operasoftware.Opera/Default/History ./db/History-opera
