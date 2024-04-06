596588e46d0cf653215f66115e084351438d252b
elzup <guild0105@gmail.com>
2024-04-07 01:12:11 +0900

    fix sqlite

--- src/read.ts
+++ src/read.ts
@@ -16,3 +16,13 @@ main()
   .finally(async () => {
     await prisma.$disconnect()
   })
+
+function convertWebkitTimestampToJsDate(webkitTimestamp: number) {
+  const unixTimestampMillis = (webkitTimestamp - 11644473600000000) / 1000
+  return new Date(unixTimestampMillis)
+}
+
+const webkitTimestamp = 13276246021066760
+const date = convertWebkitTimestampToJsDate(webkitTimestamp)
+
+console.log(date) // 出力: 実際の日付と時刻
