9f0ac7754b71bcef5026940620be3d4b4f4c220c
elzup <guild0105@gmail.com>
2024-04-07 11:04:08 +0900

    refactor

---
+++ src/history.ts
@@ -0,0 +1,62 @@
+import { Database, open } from 'sqlite'
+import sqlite3 from 'sqlite3'
+
+type RawUrl = {
+  id: number
+  url: string
+  title: string
+  visit_count: number
+  typed_count: number
+  last_visit_time: string
+  hidden: number
+}
+export type Url = Omit<RawUrl, 'last_visit_time'> & {
+  last_visit_time: Date
+}
+const isRawUrl = (url: any): url is RawUrl => {
+  return (
+    typeof url === 'object' &&
+    typeof url.id === 'number' &&
+    typeof url.url === 'string' &&
+    typeof url.title === 'string' &&
+    typeof url.visit_count === 'number' &&
+    typeof url.typed_count === 'number' &&
+    typeof url.last_visit_time === 'number' &&
+    typeof url.hidden === 'number'
+  )
+}
+
+const webkittimeToUnix = (wkTs: bigint) => {
+  const webkitTimestamp = wkTs
+  return Number(webkitTimestamp - BigInt('11644473600000000')) / 1000
+}
+
+const convertUrl = (url: RawUrl): Url => {
+  const webkitTimestamp = BigInt(url.last_visit_time)
+  const date = new Date(webkittimeToUnix(webkitTimestamp))
+  return { ...url, last_visit_time: date }
+}
+
+const GET_URLS_SQL = `
+SELECT
+  id, url, title, visit_count, typed_count, last_visit_time, hidden
+FROM urls
+WHERE last_visit_time > ? AND last_visit_time < ?
+ORDER BY last_visit_time DESC
+LIMIT ?
+`
+
+type Db = Database<sqlite3.Database, sqlite3.Statement>
+
+const getHistory = (db: Db, from: number, to: number, limit = 10000) =>
+  db.all(GET_URLS_SQL, [from, to, limit])
+
+export async function readHistory(path: string) {
+  const db = await open({ filename: path, driver: sqlite3.Database })
+  const rows = await getHistory(db, 0, 13276246021066761)
+
+  const urls = rows.filter(isRawUrl).map(convertUrl)
+
+  await db.close()
+  return urls
+}
--- src/read.ts
+++ src/read.ts
@@ -1,69 +1,14 @@
 import { writeFileSync } from 'fs'
-import { open } from 'sqlite'
-import sqlite3 from 'sqlite3'
+import { readHistory } from './history'
 
-const DB_PATH = './db/History'
+const DB_PATH = './db/History-1'
 const OUT_PATH = './dist/db.json'
 
-type RawUrl = {
-  id: number
-  url: string
-  title: string
-  visit_count: number
-  typed_count: number
-  last_visit_time: string
-  hidden: number
-}
-type Url = Omit<RawUrl, 'last_visit_time'> & {
-  last_visit_time: Date
-}
-const isRawUrl = (url: any): url is RawUrl => {
-  return (
-    typeof url === 'object' &&
-    typeof url.id === 'number' &&
-    typeof url.url === 'string' &&
-    typeof url.title === 'string' &&
-    typeof url.visit_count === 'number' &&
-    typeof url.typed_count === 'number' &&
-    typeof url.last_visit_time === 'number' &&
-    typeof url.hidden === 'number'
-  )
-}
-
-const webkittimeToUnix = (wkTs: bigint) => {
-  const webkitTimestamp = BigInt(wkTs)
-  return Number(webkitTimestamp - BigInt('11644473600000000')) / 1000000
-}
-
-const convertUrl = (url: RawUrl): Url => {
-  const webkitTimestamp = BigInt(url.last_visit_time)
-  const date = new Date(webkittimeToUnix(webkitTimestamp))
-  return { ...url, last_visit_time: date }
-}
-
-const GET_URLS_SQL = `
-SELECT
-  id,
-  url,
-  title,
-  visit_count,
-  typed_count,
-  last_visit_time,
-  hidden
-FROM urls;
-`
-
 async function main() {
-  // データベースを開く
-  const db = await open({ filename: DB_PATH, driver: sqlite3.Database })
-  const rows = await db.all(GET_URLS_SQL)
-
-  const urls = rows.filter(isRawUrl).map(convertUrl)
+  const urls = await readHistory(DB_PATH)
 
   await saveFile(urls)
   console.log(`saved: ${OUT_PATH}`)
-  // データベースを閉じる
-  await db.close()
 }
 
 const saveFile = (urls: any) => writeFileSync(OUT_PATH, JSON.stringify(urls))
